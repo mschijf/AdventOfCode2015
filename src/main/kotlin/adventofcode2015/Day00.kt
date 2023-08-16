ba3abed6d42fd35af063cc524ab2e623121cf94f
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-08-16 06:11:06 +0200

    initial commit

---
+++ src/main/kotlin/adventofcode2015/Day00.kt
@@ -0,0 +1,14 @@
+package adventofcode2016
+
+fun main() {
+    Day00(test=true).showResult()
+}
+
+class Day00(test: Boolean) : PuzzleSolverAbstract(test) {
+
+    override fun resultPartOne(): Any {
+        return super.resultPartOne()
+    }
+}
+
+
