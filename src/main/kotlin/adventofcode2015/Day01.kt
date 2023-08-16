1c9524778fe5e49ebe94d7269e6789e1229d2e92
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-08-16 06:38:38 +0200

    Day 1 - nice outlining

--- src/main/kotlin/adventofcode2015/Day01.kt
+++ src/main/kotlin/adventofcode2015/Day01.kt
@@ -1,17 +1,21 @@
 package adventofcode2016
 
 fun main() {
-    Day01(test=false).showResult()
+    Day01(test = false).showResult()
 }
 
 class Day01(test: Boolean) : PuzzleSolverAbstract(test) {
 
     override fun resultPartOne(): Any {
-        return inputLines.first().map {if (it=='(') 1 else -1 }.sum()
+        return inputLines.first()
+            .map { if (it == '(') 1 else -1 }
+            .sum()
     }
 
     override fun resultPartTwo(): Any {
-        return inputLines.first().runningFold(0) { acc, c -> if (c=='(') acc+1 else acc-1 }.indexOfFirst { it == -1 }
+        return inputLines.first()
+            .runningFold(0) { acc, c -> if (c == '(') acc + 1 else acc - 1 }
+            .indexOfFirst { it == -1 }
     }
 }
 
