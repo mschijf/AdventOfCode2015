eeda06e0dff71303957ec0ae2cfc9c559aaf2ec1
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-08-20 18:23:58 +0200

    Day 11 - refactored

--- src/main/kotlin/adventofcode2015/Day11.kt
+++ src/main/kotlin/adventofcode2015/Day11.kt
@@ -32,13 +32,8 @@ class Day11(test: Boolean) : PuzzleSolverAbstract(test) {
     private fun Char.isLegal() =
         this in "abcdefghjkmnpqrstuvwxyz"
 
-    private fun String.hasThreeInARow(): Boolean {
-        for (i in 2..< this.length) {
-            if (this[i-2] == this[i-1]-1 && this[i-1] == this[i]-1)
-                return true
-        }
-        return false
-    }
+    private fun String.hasThreeInARow() =
+        this.windowed(3).any{ it[0] == it[1]-1 && it[1] == it[2]-1 }
 
     private fun String.hasTwoTwinLetters(): Boolean {
         var count = 0
