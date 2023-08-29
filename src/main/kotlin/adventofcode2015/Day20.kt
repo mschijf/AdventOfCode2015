b61d87204691b79968a970a7661325466a34f962
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-08-29 18:59:28 +0200

    Day 20 - refactored

--- src/main/kotlin/adventofcode2015/Day20.kt
+++ src/main/kotlin/adventofcode2015/Day20.kt
@@ -14,45 +14,39 @@ class Day20(test: Boolean) : PuzzleSolverAbstract(test) {
     private val puzzleInput = if (test) 150 else 34_000_000
 
     override fun resultPartOne(): Any {
-        val houseList = deliverPresents(1_000_000)
-        return houseList.indexOfFirst { it >= puzzleInput }
+        return deliverPresents(10).indexOfFirst { it >= puzzleInput }
     }
 
     override fun resultPartTwo(): Any {
-        val houseList = deliverPresentsPart2(1_000_000)
-        return houseList.indexOfFirst { it >= puzzleInput }
+        return deliverPresents( 11, 50).indexOfFirst { it >= puzzleInput }
     }
 
-    private fun deliverPresents(numberOfHouses: Int): IntArray {
-        val houseList = IntArray(numberOfHouses)
-        for (elfNr in 1..< numberOfHouses) {
-            for (houseNr in elfNr..< numberOfHouses step elfNr) {
-                houseList[houseNr] += elfNr*10
-            }
+    private fun deliverPresents(presentsPerHouse: Int, maxHouses: Int = -1): IntArray {
+        val houseList = IntArray(houseArraySize)
+        for (elfNr in 1..< houseList.size) {
+            houseList.deliverPresentsBy(elfNr, presentsPerHouse, maxHouses)
         }
         return houseList
     }
 
-    private fun deliverPresentsPart2(numberOfHouses: Int): IntArray {
-        val houseList = IntArray(numberOfHouses)
-        for (elfNr in 1..< numberOfHouses) {
-            var countPresents = 0
-            for (houseNr in elfNr..< numberOfHouses step elfNr) {
-                houseList[houseNr] += elfNr*11
-                countPresents++
-                if (countPresents == 50)
-                    break
-            }
+    private fun IntArray.deliverPresentsBy(elfNumber: Int, presentsPerHouse: Int, maxHouses: Int) {
+        var count = maxHouses
+        for (houseNr in elfNumber..< this.size step elfNumber) {
+            this[houseNr] += elfNumber * presentsPerHouse
+            if (count-- == 0)
+                return
         }
-        return houseList
     }
 
+    companion object {
+        const val houseArraySize: Int = 1_000_000
+    }
 
     // als je het aantal pakjes per huis wilt berekenen zonder zeef-constructie
     //        return generateSequence(1){it+1}.first { it.houseToPresent() >= puzzleInput }
-    private fun Int.houseToPresent(): Int {
-        return (1..this).filter{this % it == 0}.sum()*10
-    }
+//    private fun Int.houseToPresent(): Int {
+//        return (1..this).filter{this % it == 0}.sum()*10
+//    }
 
 }
 
